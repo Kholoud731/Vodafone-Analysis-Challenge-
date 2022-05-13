dacb3e75912f60b9d15c013a8d42b4354fc7c6c5
Kholoud731 <>
2022-05-13 23:52:28 +0200

    implementing the switch function

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,10 +1,45 @@
 
+import { useEffect, useRef, useState } from 'react';
+
+
+
 const ThemeSwitch = ()=>{
+
+    const switchRef = useRef<HTMLInputElement>(null)
+    const [theme, setTheme] = useState('')
+
+    useEffect(()=>{
+        
+        if(window.localStorage.getItem('theme') && switchRef.current?.className !== 'active'){
+            setTheme('Dark')
+            switchRef.current?.classList.add("active")
+            switchRef.current?.click()
+        }
+
+
+    },[theme])
+
+    const changetheme = (e: React.MouseEvent<HTMLInputElement, MouseEvent>)=>{
+        if(theme && switchRef.current?.className === 'active'){
+            window.localStorage.removeItem('theme')
+            switchRef.current?.classList.remove("active")
+            setTheme('')
+        }else if(theme && switchRef.current?.className !== 'active'){
+            window.localStorage.setItem('theme', 'Dark')
+            switchRef.current?.classList.add("active")
+
+        }else{
+            window.localStorage.setItem('theme', 'Dark')
+            setTheme('Dark')
+        }
+    }
+
+
     return (
         <div className='theme'>
         <span className='dark'> Dark Mode </span> 
      <label htmlFor='switch' className="switch">
-     <input id="switch" type="checkbox"/>
+     <input id="switch" type="checkbox" onClick={(e)=>changetheme(e)} ref={switchRef}/>
      <span className="slider round"></span>
      </label>
      </div>
